cb9ec5916eebc4ec25039982ddfda73a338b782a
aleksey_geshko <aleksey_geshko@rambler.ru>
2025-03-23 12:57:12 +0300

    GKA-11: Сделал сервер асинхронным

--- server/server.py
+++ server/server.py
@@ -1,11 +1,9 @@
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
-import subprocess
 import structlog
-import uvicorn
 import json
-
+import asyncio
 from logging_conf import configure_logging
 
 logger = configure_logging()
@@ -46,26 +44,29 @@ async def calculate(request: Request, float: bool = False):
     if float:
         cmd.append("--float")
 
-    logger.info("executing_subprocess", cmd=cmd, expression=expression)
+    logger.info("executing_subprocess_async", cmd=cmd, expression=expression)
 
     try:
-        result = subprocess.run(
-            cmd,
-            input=expression,
-            text=True,
-            capture_output=True
+        process = await asyncio.create_subprocess_exec(
+            *cmd,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
         )
+
+        stdout, stderr = await process.communicate(input=expression.encode())
+
+        if process.returncode != 0:
+            logger.error("calc_error", stderr=stderr.decode().strip())
+            return JSONResponse(
+                status_code=500,
+                content={"error": stderr.decode().strip()}
+            )
+
+        output = stdout.decode().strip()
+        logger.info("calc_success", output=output)
+        return JSONResponse(content=output)
+
     except Exception as e:
         logger.exception("subprocess_error", error=str(e))
         return JSONResponse(status_code=500, content={"error": f"Subprocess error: {e}"})
-
-    if result.returncode != 0:
-        logger.error("calc_error", stderr=result.stderr.strip())
-        return JSONResponse(
-            status_code=500,
-            content={"error": result.stderr.strip()}
-        )
-
-    output = result.stdout.strip()
-    logger.info("calc_success", output=output)
-    return JSONResponse(content=output)
